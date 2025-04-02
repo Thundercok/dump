2cadd4709f698c9f570c213d191bb35802ebf6ad
Hài Dón <cathuys69@gmail.com>
2025-04-02 14:24:05 +0700

    lab3

--- labs/cong-nghe-phan-mem/Lab3/Home/WinformUI/DAL/CustomerRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿ using DAL;
-public class CustomerRepository {
-    private readonly Exer1DbContext _context;
-    public CustomerRepository(Exer1DbContext context) => _context = context;
-
-    public List<Customer> GetAll() => _context.Customers.ToList(); // [cite: 211, 214]
-    public void Add(Customer c) { _context.Customers.Add(c); _context.SaveChanges(); } // [cite: 202, 206]
-}
