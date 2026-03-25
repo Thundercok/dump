6a728554971f265d2f6f0920216c8e1f6a5c58bd
Hài Dón <cathuys69@gmail.com>
2026-03-26 02:08:39 +0700

    removed j4f

--- mixi88-j4f/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express          = require('express');
-const router           = express.Router();
-const AdminController  = require('../controllers/adminController');
-
-router.get('/',          AdminController.dashboard);
-router.get('/api/stats', AdminController.liveStats);
-
-module.exports = router;
--- mixi88-j4f/routes/game.js
+++
@@ -1,9 +0,0 @@
-const express        = require('express');
-const router         = express.Router();
-const GameController = require('../controllers/gameController');
-
-router.get('/',        GameController.index);
-router.post('/roll',   GameController.roll);
-router.get('/history', GameController.history);
-
-module.exports = router;
