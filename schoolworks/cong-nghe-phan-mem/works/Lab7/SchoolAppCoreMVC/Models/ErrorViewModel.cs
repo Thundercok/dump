5c00c6470649a06bc6ed87bd2c40c67843a51a01
Hài Dón <cathuys69@gmail.com>
2026-05-14 21:54:48 +0700

    lab7

--- schoolworks/cong-nghe-phan-mem/works/Lab7/SchoolAppCoreMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SchoolAppCoreMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ schoolworks/lap-trinh-huong-doi-tuong/Final/Vehicle.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehicle {
+    private String brand;
+    private String model;
+    private final int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public static class Car extends Vehicle {
+        private int numberOfDoors;
+        private int fuelType;
+
+        public Car(String brand, String model, int year, int numberOfDoors, int fuelType) {
+            super(brand, model, year);
+            this.numberOfDoors = numberOfDoors;
+            this.fuelType = fuelType;
+        }
+
+        public int getNumberOfDoors() {
+            return numberOfDoors;
+        }
+
+        public int getFuelType() {
+            return fuelType;
+        }
+
+        public void setNumberOfDoors(int numberOfDoors) {
+            this.numberOfDoors = numberOfDoors;
+        }
+
+        public void setFuelType(int fuelType) {
+            this.fuelType = fuelType;
+        }
+
+    }
+
+    public static class Motorcycle extends Vehicle {
+        private int engineCapacity;
+        private boolean hasSidecar;
+
+        public Motorcycle(String brand, String model, int year, int engineCapacity, boolean hasSidecar) {
+            super(brand, model, year);
+            this.engineCapacity = engineCapacity;
+            this.hasSidecar = hasSidecar;
+        }
+
+        public int getEngineCapacity() {
+            return engineCapacity;
+        }
+
+        public boolean getHasSidecar() {
+            return hasSidecar;
+        }
+
+        public void setEngineCapacity(int engineCapacity) {
+            this.engineCapacity = engineCapacity;
+        }
+
+        public void setHasSidecar(boolean hasSidecar) {
+            this.hasSidecar = hasSidecar;
+        }
+    }
+
+    public static List<Vehicle> search(List<Vehicle> list, int year) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : list) {
+            if (vehicle.getYear() > year) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Vehicle> list = new ArrayList<>();
+        list.add(new Car("BMW", "NhatHuy", 2004, 4, 1));
+        list.add(new Car("Mecxedec", "XiVit", 2012, 4, 1));
+        list.add(new Motorcycle("Honda", "Cut", 2022, 150, false));
+        list.add(new Motorcycle("Honda", "Past", 2002, 150, false));
+        list.add(new Motorcycle("Honda", "Future", 2026, 150, false));
+        list.add(new Motorcycle("Suzuki", "Elegant", 2022, 500, false));
+        list.add(new Motorcycle("Suzuki", "Inelegant", 2024, 500, false));
+
+        List<Vehicle> result = search(list, 2020);
+        for (Vehicle vehicle : result) {
+            System.out.println(vehicle.getBrand() + " " + vehicle.getModel() + " " + vehicle.getYear());
+        }
+    }
+}
